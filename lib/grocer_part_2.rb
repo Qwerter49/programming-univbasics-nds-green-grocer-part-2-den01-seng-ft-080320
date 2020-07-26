9937eeb241e56c5f6f111a813a5a5b408d745a86
Jay Oliver <jaykamas@gmail.com>
2020-07-26 21:28:09 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -14,7 +14,7 @@ def apply_coupons(cart, coupons)
     else 
       cart_item_with_coupon = {
         :item => couponed_item_name,
-        :price => coupons[i][:price]
+        :price => coupons[i][:cost] 
       }
     end
   end
