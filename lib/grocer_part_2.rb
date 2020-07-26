1a449548893421c4561a6a5494b2fbc00f2649a4
Jay Oliver <jaykamas@gmail.com>
2020-07-26 21:31:02 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -19,7 +19,7 @@ def apply_coupons(cart, coupons)
         :clearance => cart_item[:clearance]
       }
       cart << cart_item_with_coupon
-      
+      cart_item_with_coup
     end
   end
   i += 1 
